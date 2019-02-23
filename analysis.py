ca114681f486a73b13b352c0d67164ae491b69d2
Peter Grimshaw <pagrimshaw@gmail.com>
2019-02-23 08:13:25 +0000

    Initial analysis

---
+++ analysis.py
@@ -0,0 +1,6 @@
+# This file contains various experiments run on the Q-learning algorithm defined in the
+# FrozenQLearner class
+
+from FrozenQLearner import FrozenQLearner
+
+fql = FrozenQLearner(alpha=)
